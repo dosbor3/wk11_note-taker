bc6581bce09d6062bea47d1a7805e6796aabf687
I am Billie <dosbor3@gmail.com>
2022-02-13 11:09:25 -0600

    tested POST and GET method for notes.json

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const noteRoutes = require("../apiRoutes/noteRoutes");
-router.use(noteRoutes);
+const animalRoutes = require('../apiRoutes/noteRoutes');
+
+router.use(animalRoutes);
 
 module.exports = router;
